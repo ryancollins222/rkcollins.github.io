1c4c1a72dc7ad70623434997dd869a74d1ece029
Ryan Collins <ryan.k.collins222@gmail.com>
2020-05-13 09:36:36 -0700

    Updates

---
+++ precache-manifest.24ddddcfe4e4a209637bb1c235ddeebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f326bf07b910341edb2f8c0294e6ce00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a5ac8eb93a91b50e26",
+    "url": "/static/css/main.7ca7c303.chunk.css"
+  },
+  {
+    "revision": "c9001420fd6b14c7c739",
+    "url": "/static/js/2.23f2db47.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.23f2db47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a5ac8eb93a91b50e26",
+    "url": "/static/js/main.1fba2578.chunk.js"
+  },
+  {
+    "revision": "eb13f1f26b842bbf973e",
+    "url": "/static/js/runtime-main.308710f9.js"
+  }
+]);
