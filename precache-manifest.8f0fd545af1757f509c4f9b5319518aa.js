ecd796f6c588019faf2ff56eca45272673388532
Ryan Collins <ryan.k.collins222@gmail.com>
2020-05-12 17:34:59 -0700

    Updates

---
+++ precache-manifest.8f0fd545af1757f509c4f9b5319518aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc8f9ee64e27db1881b210d73b667e1",
+    "url": "/ryancollins222.github.io/index.html"
+  },
+  {
+    "revision": "87199f8f44e78fe31898",
+    "url": "/ryancollins222.github.io/static/css/main.fe87e4b2.chunk.css"
+  },
+  {
+    "revision": "9c5633187d56a039495c",
+    "url": "/ryancollins222.github.io/static/js/2.20c8b35e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ryancollins222.github.io/static/js/2.20c8b35e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87199f8f44e78fe31898",
+    "url": "/ryancollins222.github.io/static/js/main.03fca0f6.chunk.js"
+  },
+  {
+    "revision": "575576ddda309400577d",
+    "url": "/ryancollins222.github.io/static/js/runtime-main.39e49d0a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87be6904181a35d0aed9cb1ec2d1c081.js"
+  "/ryancollins222.github.io/precache-manifest.8f0fd545af1757f509c4f9b5319518aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ryancollins222.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
